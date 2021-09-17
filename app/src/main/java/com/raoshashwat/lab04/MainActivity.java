6b9cc51b0dfa5bcba1d56e4768678239436c2519
Shash_Rao <shashwat.a.rao@gmail.com>
2021-09-17 13:46:38 -0400

    Final without embellishments

--- app/src/main/java/com/raoshashwat/lab04/MainActivity.java
+++ app/src/main/java/com/raoshashwat/lab04/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout6);
+        setContentView(R.layout.layout7);
     }
 }
